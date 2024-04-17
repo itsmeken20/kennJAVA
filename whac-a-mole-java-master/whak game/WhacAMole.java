164d9deaea5cc44d3d6a226c451dba56c2935c95
itsmeken20 <123226533+itsmeken20@users.noreply.github.com>
2024-04-17 16:18:23 +0800

    Add files via upload

---
+++ whac-a-mole-java-master/whak game/WhacAMole.java
@@ -0,0 +1,128 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*;
+
+public class WhacAMole {
+    int boardWidth = 700;
+    int boardHeight = 650;
+
+    JFrame frame = new JFrame("Whac A Mole");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel(); 
+	
+    JButton[] board = new JButton[10];
+    ImageIcon moleIcon;
+    ImageIcon plantIcon;
+
+    JButton currMoleTile;
+    JButton currPlantTile;
+
+    Random random = new Random();
+    Timer setMoleTimer;
+    Timer setPlantTimer;
+    int score = 0;
+
+    WhacAMole() {
+    
+	frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+	textLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+	textLabel.setHorizontalAlignment(JLabel.CENTER);
+	textLabel.setText("Score: " + Integer.toString(score));
+	textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);		
+        frame.add(textPanel, BorderLayout.NORTH);
+
+	boardPanel.setLayout(new GridLayout(3, 3));
+      
+        frame.add(boardPanel);
+
+        
+        Image plantImg = new ImageIcon(getClass().getResource("./piranha.png")).getImage();
+        plantIcon = new ImageIcon(plantImg.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
+
+        Image moleImg = new ImageIcon(getClass().getResource("./monty.png")).getImage();
+        moleIcon = new ImageIcon(moleImg.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
+		
+	for (int i = 0; i < 10; i++) {
+            JButton tile = new JButton();
+            board[i] = tile;
+            boardPanel.add(tile);
+            tile.setFocusable(false);
+  
+
+            tile.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton tile = (JButton) e.getSource();
+                    if (tile == currMoleTile) {
+                        score += 10;
+                        textLabel.setText("Score: " + Integer.toString(score));
+                    }
+                    else if (tile == currPlantTile) {
+		                textLabel.setText("Game Over: " + Integer.toString(score));
+                        setMoleTimer.stop();
+                        setPlantTimer.stop();
+                        for (int i = 0; i < 9; i++) {
+                            board[i].setEnabled(false);
+                        }
+                    }
+                }
+            });
+	}
+
+        setMoleTimer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            
+                if (currMoleTile != null) {
+                    currMoleTile.setIcon(null);
+                    currMoleTile = null;
+                }
+
+                
+                int num = random.nextInt(9); 
+                JButton tile = board[num];
+
+        
+                if (currPlantTile == tile) return;
+
+             
+                currMoleTile = tile;
+                currMoleTile.setIcon(moleIcon);
+            }
+        });
+
+        setPlantTimer = new Timer(1500, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+    
+                if (currPlantTile != null) {
+                    currPlantTile.setIcon(null);
+                    currPlantTile = null;
+                }
+
+         
+                int num = random.nextInt(9);
+                JButton tile = board[num];
+
+                if (currMoleTile == tile) return;
+
+                
+                currPlantTile = tile;
+                currPlantTile.setIcon(plantIcon);
+            }
+        });
+
+        setMoleTimer.start();
+        setPlantTimer.start();
+        frame.setVisible(true);
+    }
+}
+
+
